3265aa31634773661e7cdcf8f81bcf0913a8b88d
TheNinerSix <wjclayton96@gmail.com>
2018-04-02 08:11:44 -0500

    Code Fixes
    Some syntax changes for correctness, and added two import statements to course_app/models.py.

--- CodingBotSite/game_app/models.py
+++ CodingBotSite/game_app/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-Class Pack(models.Model):
+class Pack(models.Model):
     topic = models.CharField(max_length=100)
     description = models.CharField(max_length=2000)
     fininshed = models.IntegerField(default=0)   
 
-Class Problem(models.Model):
+class Problem(models.Model):
     probQuestion = models.CharField(max_length=2000)
     probAnswer = models.CharField(max_length=1000)
     story = models.CharField(max_length=5000)
-    packId = models.ForeignKey(Pack, on_delete=models.CASCADE())
+    packId = models.ForeignKey(Pack, on_delete=models.CASCADE)
