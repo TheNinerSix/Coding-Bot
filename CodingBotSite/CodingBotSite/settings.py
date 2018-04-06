4566e02a02d4513e89e7a8e60d9a6f2439009366
Will Clayton <wjclayton96@gmail.com>
2018-04-06 11:32:35 -0500

    Database Migrations
    Added models to Django INSTALLED_APPS, and applied migrations.

--- CodingBotSite/CodingBotSite/settings.py
+++ CodingBotSite/CodingBotSite/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'game_app.apps.GameAppConfig',
+    'course_app.apps.CourseAppConfig',
+    'accounts_app.apps.AccountsAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
